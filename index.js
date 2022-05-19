3f4d290c59c50cb3228cd8d8665946c2b8333738
Paulo Almeida <paulocomy1408@gmail.com>
2022-05-19 19:59:49 -0400

    jogo finalizado

--- index.js
+++ index.js
@@ -1,142 +1,167 @@
-const displayPlayerChoice = document.querySelector('#playerChoice')
-const displayPcChoice = document.querySelector('#pcChoice')
+const playerPanel = document.querySelector('.playerPanel > img')
+const playerOptions = document.querySelectorAll('.playerOptions > .btn')
 
-const playerChoices = document.querySelectorAll('.playerChoices > .btn')
-const pcChoices = document.querySelectorAll('.pcChoices > .btn > img')
+const machinePanel = document.querySelector('.machinePanel > img')
+const machineOptions = document.querySelectorAll('.machineOptions > .btn > img')
 
-const pcChoicesBtn = document.querySelectorAll('.pcChoices > .btn')
+const pcChoicesBtn = document.querySelectorAll('.machineOptions > .btn')
 
 const result = document.querySelector('.result')
 const winner = document.querySelector('.winner')
 
 //pega o nó filho do botão clicado neste caso o <img> e passa o seu src como parámetro da função showPlayerChoice e
 //também passa como parámetro da função playRound o atributo alt da imagem clicado. ou seja. pedra, papel ou tesoura
-let getPlayerChoice = (e) => {
-    let btn = e.currentTarget
-    activeChoice(playerChoices, btn)
-    let img = btn.children
-    let playerChoice = img[0].getAttribute("alt")
-    showChoice(displayPlayerChoice, img[0].src)
-    playRound(playerChoice)
+
+let getPlayerPick = (e) => {
+  let btn = e.currentTarget
+  let img = btn.children
+
+  displayActivePick(playerOptions, btn)
+
+  let playerPick = img[0].getAttribute("alt")
+  displayPick(playerPanel, img[0].src)
+
+  playRound(playerPick)
 }
 
-let getPcChoice = (computerChoice) => {
-    console.log(pcChoices.parentNode)
-    pcChoices.forEach(choice => {
-        if(computerChoice == choice.getAttribute('alt')) {
-            showChoice(displayPcChoice, choice.src)
-            activeChoice(pcChoicesBtn, choice.parentNode)
-        }
-    })
+let displayActivePick = (options, btn) => {
+  let optionsArray = Array.from(options)
+  
+  optionsArray.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  
+  setTimeout( () => btn.classList.add('active'), 100 ) 
 }
 
 //recebe como parámetro um src e altera o src da imagem da escolha do usuário
-let showChoice = (display, src) => {
-    return display.src = src
+let displayPick = (panel, src) => {
+  return panel.src = src
 }
 
-//adiciona escutador de eventos em cada botão de escolha do jogador e chama a função getPlayerChoice
-let playerChoice = playerChoices.forEach(btn => {
-    btn.addEventListener('click', getPlayerChoice)
-})
+let options = ['rock', 'paper', 'scissors']
 
-let activeChoice = (array, btn) => {
-    let arr = Array.from(array)
+const machinePlay = () => options[Math.floor(Math.random() * 3)]
 
-    arr.forEach(btn => {
-        btn.classList.remove('active')
-    })
-
-    setTimeout(() => btn.classList.add('active'), 100) 
+let displayMachinePick = (machinePick) => {
+  machineOptions.forEach(option => {
+    if(machinePick == option.getAttribute('alt')) {
+      displayPick(machinePanel, option.src)
+      displayActivePick(pcChoicesBtn, option.parentNode)
+    }
+  })
 }
-
-let choices = ['rock', 'paper', 'scissors']
-
-const computerPlay = () => choices[Math.floor(Math.random() * 3)]
 
 let playerScoreCount = 0
 let pcScoreCount = 0
 
 const playerScore = document.querySelector('#playerScore')
 const pcScore = document.querySelector('#pcScore')
+const gameWinner = document.querySelector('.gameWinner')
 
-const playRound = (playerChoice) => {
-    let computerChoice = computerPlay()
-    getPcChoice(computerChoice)
-    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)
-    console.log(playerChoice)
-    computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-
-    switch (playerChoice) {
-        case 'Rock':
-            if (computerChoice === 'Scissors') {
-                result.innerText = `${playerChoice} and ${computerChoice}`
-                winner.innerText = 'Winner is: Player'
-                playerScoreCount++;
-                playerScore.innerText = playerScoreCount
-            }
-            else if (computerChoice === playerChoice) {
-                result.innerText = `${playerChoice} and ${computerChoice}. It's a tie.`
-                winner.innerText = 'Winner is:'
-            } else {
-                result.innerText = `${playerChoice} and ${computerChoice}`
-                winner.innerText = 'Winner is: Machine'
-                pcScoreCount++
-                pcScore.innerText = pcScoreCount
-            }
-        break;
-        case 'Paper':
-            if (computerChoice === 'Rock') {
-                result.innerText = `${playerChoice} and ${computerChoice}`
-                winner.innerText = 'Winner is: Player'
-                playerScoreCount++;
-                playerScore.innerText = playerScoreCount
-            } 
-            else if (computerChoice === playerChoice) {
-                result.innerText = `${playerChoice} and ${computerChoice}. It's a tie.`
-                winner.innerText = 'Winner is:'
-            } else {
-                result.innerText = `${playerChoice} and ${computerChoice}.`
-                winner.innerText = 'Winner is: Machine'
-                pcScoreCount++
-                pcScore.innerText = pcScoreCount
-            }
-        break;
-        case 'Scissors':
-            if (computerChoice === 'Paper') {
-                result.innerText = `${playerChoice} and ${computerChoice}`
-                winner.innerText = 'Winner is: Player'
-                playerScoreCount++;
-                playerScore.innerText = playerScoreCount
-            } 
-            
-            else if (computerChoice === playerChoice) {
-                result.innerText = `${playerChoice} and ${computerChoice}. It's a tie.`
-                winner.innerText = 'Winner is:'
-            } else {
-                result.innerText = `${playerChoice} and ${computerChoice}.`
-                winner.innerText = 'Winner is: Machine'
-                pcScoreCount++
-                pcScore.innerText = pcScoreCount
-            }
-        break;
-    }
-
+const checkWinner = (playerScore, machineScore, displayPlayerScore, displayMachineScore) => {
+  if(playerScore == 5) {
+    gameWinner.innerText = 'Game Winner is You!'
+    playerScore = 0
+    machineScore = 0
+    displayPlayerScore.innerText = playerScore
+    displayMachineScore.innerText = machineScore
+  }
+  if(machineScore == 5) {
+    gameWinner.innerText = 'Game Winner is Machine!'
+    playerScore = 0
+    machineScore = 0
+    displayPlayerScore.innerText = playerScore
+    displayMachineScore.innerText = machineScore
+  } 
 }
 
+const playRound = (playerPick) => {
 
-    // for (let i = 0; i < 5; i++) {
-    //     let playerChoice = playerPlay()
-    //     let computerChoice = computerPlay()
-    //     playRound(playerChoice, computerChoice)
-    // }
-    
-    // if (playerScoreCount > computerScore) {
-    //     console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}. Player is the Winner`)
-    // } else if (playerScore == computerScore) {
-    //     console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}. Draw`)
-    // } else {
-    //     console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}. Computer is the Winner`)
-    // }
+  let machinePick = machinePlay()
+  displayMachinePick(machinePick)
 
-// }
+  playerPick = playerPick.charAt(0).toUpperCase() + playerPick.slice(1)
+  machinePick = machinePick.charAt(0).toUpperCase() + machinePick.slice(1)
+
+  
+  switch (playerPick) {
+    case 'Rock':
+      if (machinePick === 'Scissors') {
+        result.innerText = `${playerPick} and ${machinePick}`
+        winner.innerText = 'Round winner is: Player'
+        playerScoreCount++;
+        playerScore.innerText = playerScoreCount
+      }
+      else if (machinePick === playerPick) {
+        result.innerText = `${playerPick} and ${machinePick}. It's a tie.`
+        winner.innerText = 'Round winner is:'
+      } else {
+        result.innerText = `${playerPick} and ${machinePick}`
+        winner.innerText = 'Round winner is: Machine'
+        pcScoreCount++
+        pcScore.innerText = pcScoreCount
+      }
+    break;
+    case 'Paper':
+      if (machinePick === 'Rock') {
+        result.innerText = `${playerPick} and ${machinePick}`
+        winner.innerText = 'Round winner is: Player'
+        playerScoreCount++;
+        playerScore.innerText = playerScoreCount
+      } 
+      else if (machinePick === playerPick) {
+        result.innerText = `${playerPick} and ${machinePick}. It's a tie.`
+        winner.innerText = 'Round winner is:'
+      } else {
+        result.innerText = `${playerPick} and ${machinePick}.`
+        winner.innerText = 'Round winner is: Machine'
+        pcScoreCount++
+        pcScore.innerText = pcScoreCount
+      }
+    break;
+    case 'Scissors':
+      if (machinePick === 'Paper') {
+        result.innerText = `${playerPick} and ${machinePick}`
+        winner.innerText = 'Round winner is: Player'
+        playerScoreCount++;
+        playerScore.innerText = playerScoreCount
+      }  
+      else if (machinePick === playerPick) {
+        result.innerText = `${playerPick} and ${machinePick}. It's a tie.`
+        winner.innerText = 'Round winner is:'
+      } else {
+        result.innerText = `${playerPick} and ${machinePick}.`
+        winner.innerText = 'Round winner is: Machine'
+        pcScoreCount++
+        pcScore.innerText = pcScoreCount
+      }
+    break;
+  }
+
+  checkWinner(playerScoreCount, pcScoreCount, playerScore, pcScore)
+
+}
+          
+          
+          // for (let i = 0; i < 5; i++) {
+            //     let playerChoice = playerPlay()
+            //     let computerChoice = computerPlay()
+            //     playRound(playerChoice, computerChoice)
+            // }
+            
+            // if (playerScoreCount > computerScore) {
+              //     console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}. Player is the Winner`)
+              // } else if (playerScore == computerScore) {
+                //     console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}. Draw`)
+                // } else {
+                  //     console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}. Computer is the Winner`)
+                  // }
+                  
+                  // }
+                  
+                  //adiciona escutador de eventos em cada botão de escolha do jogador e chama a função getPlayerChoice   
+                  
+playerOptions.forEach(option => {
+option.addEventListener('click', getPlayerPick)
+})
